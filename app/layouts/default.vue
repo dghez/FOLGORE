cee34e499e2c1050578750f4ec08a9468381f100
Roberto Borghesi <dborghez@yahoo.it>
2025-10-29 18:05:48 +0100

    [CSS]: Add --vh calculations

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,3 +1,17 @@
+<script setup>
+import { EVENTS } from '@js/events'
+const { $resize } = useNuxtApp()
+
+const onResize = () => {
+    document.documentElement.style.setProperty(
+        '--vh', `${$resize.wh / 100}px`
+    )
+}
+
+// EVENTS
+useEvent(EVENTS.APP_RESIZE, onResize)
+</script>
+
 <template>
   <div>
     <LibraryIntro />
