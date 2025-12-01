58ab291f82b6323a8cae6aca53eff0f0b30645a8
Roberto Borghesi <dborghez@yahoo.it>
2025-12-01 11:59:07 +0100

    [SEO]: Add seo component

---
+++ app/composables/useSeo.js
@@ -0,0 +1,51 @@
+const BASE_URL = "https://batcloud.art"
+const DESCRIPTION = 'The bat cloud: a moment of encounter between bats and humans at sfer ik museion'
+const IMAGE = BASE_URL + '/share.jpg'
+const BASE_TITLE_TEMPLATE = 'THE BAT CLOUD'
+
+export const useSeo = (
+    { title = '', description = '', image = null } = {}, // ? seo
+    pageTitle = '',
+    slug = undefined
+) => {
+
+    const route = useRoute()
+    const finalSlug = slug || route.fullPath
+
+    // METE
+    const descriptions = [
+        { name: 'description', hid: 'description', content: description || DESCRIPTION },
+        { property: 'og:description', hid: 'og_description', content: description || DESCRIPTION },
+        { property: 'og:image:alt', hid: 'og_image_alt', content: description || DESCRIPTION },
+        { name: 'twitter:description', hid: 'twitter_description', content: description || DESCRIPTION },
+        { name: 'twitter:image:alt', hid: 'twitter_image_alt', content: description || DESCRIPTION },
+    ]
+
+    // IMAGES
+    const images = [
+        { property: 'og:image', hid: 'og_image', content: image || IMAGE },
+        { property: 'og:image:secure_url', hid: 'og_image_secure', content: image || IMAGE },
+        { name: 'twitter:image:src', hid: 'twitter_image', content: image || IMAGE },
+        { name: 'twitter:image', content: image || IMAGE },
+    ]
+
+    // TITLES
+    const titles = [
+        { name: 'keywords', content: `${BASE_TITLE_TEMPLATE} ${title || pageTitle}` },
+        { property: 'og:title', hid: 'og_title', content: `${BASE_TITLE_TEMPLATE} ${title || pageTitle}` },
+        { name: 'twitter:title', hid: 'twitter_title', content: `${BASE_TITLE_TEMPLATE} ${title || pageTitle}` },
+    ]
+
+    // HEAD
+    useHead({
+        title: `${BASE_TITLE_TEMPLATE} ${title || pageTitle}`,
+        meta: [
+            ...(title || pageTitle ? titles : []),
+            ...(description || DESCRIPTION ? descriptions : []),
+            ...(image || IMAGE ? images : []),
+        ],
+        link: [
+            { rel: 'canonical', href: `${BASE_URL}${finalSlug}` },
+        ],
+    })
+}
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,69 @@ export default defineNuxtConfig({
     }
   },
 
-  modules: ['@nuxt/eslint'],
+  // SEO
+  site: {
+    url: 'https://batcloud.art/',
+    name: 'THE BAT CLOUD',
+  },
+
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=device-width, initial-scale=1',
+      // title: TITLE,
+      meta:  [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'format-detection', content: 'telephone=no' },
+        { name: 'robots', content: 'index, follow' },
+        // { name: 'description', content: DESCRIPTION },
+
+        // { name: 'author', content: TITLE},
+        { name: 'robots', content: 'index, follow' },
+
+        // { name: 'twitter:creator', content: CREATOR },
+        // { name: 'twitter:url', content: URL },
+
+        // { property: 'og:title', content: TITLE},
+        // // { property: 'og:site_name', content: TITLE},
+        // { property: 'og:description', content: DESCRIPTION},
+        { property: 'og:locale', content: 'en_gb' },
+        { property: 'og:type', content: 'website' },
+        // { property: 'og:url', content: URL},
+
+        // { property: 'og:image', content: `${URL}/share.jpg` },
+        // { property: 'og:image:secure_url', content: `${URL}/share.jpg` },
+        // { property: 'og:image:type', content: 'image/jpeg' },
+        // { property: 'og:image:width', content: '1200' },
+        // { property: 'og:image:height', content: '630' },
+
+        // { name: 'twitter:image', content: `${URL}/share.jpg` },
+        // { name: 'twitter:card', content: 'summary_large_image' },
+
+        // { name: 'theme-color', content: MAIN_COLOR },
+        // { name: 'msapplication-TileColor', content: MAIN_COLOR },
+      ],
+      htmlAttrs: {
+        lang: 'en'
+      },
+      link: [
+        // GENERATED USING https://realfavicongenerator.net/
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '48x48', href: '/favicon-48x48.png' },
+        { rel: 'icon', type: 'image/svg+xml', sizes: '16x16', href: '/favicon.svg' },
+        { rel: 'shortcut icon', href: '/favicon.ico' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+        // { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: MAIN_COLOR },
+      ],
+    },
+  },
+
+  modules: [
+    '@nuxtjs/sitemap',
+    '@nuxt/eslint'
+  ],
+
   eslint: {
     config: {
       stylistic: false // <---
